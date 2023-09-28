3e3efeea81d2b73177d70c5df68f8379e6c4d82b
SeyidaliAgayev <seyidali.aga@div.edu.az>
2023-09-28 13:27:23 +0400

    binary search algorithm created

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] intArray = {10, 4, 7, 5, 9, 0, 23};
+        int index = BinarySearchAlgorithm.binarySearch(intArray, 0);
+        System.out.println("Index of target is: " + index);
+
+    }
+
+}
